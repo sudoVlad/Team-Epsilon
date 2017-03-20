957ee238fa872d73901dda1312f10fc570a247f0
tangct197 <christhtang@gmail.com>
2017-03-19 20:53:07 -0400

    Finished contact page, it is unit tested, made a forms.py file for the contact page, adjusted contact.html to use django forms, and touched the settings.py file adding the code to setup a emailbackend if needed

--- avidalab/contact/tests.py
+++ avidalab/contact/tests.py
@@ -2,21 +2,29 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from django import forms
+from contact.views import contact
+from .forms import ContactForm
 
 # Create your tests here.
+# Tests for the Contact Page
 class ContactPageTest(TestCase):
-
+    def test_contact_form(self):
+        form_data = {'name': 'some name', 'phone': '111-111-1111',
+                        'email': 'example@example.com',
+                        'subject': 'some subject', 'message': 'some message'}
+        form = ContactForm(data = form_data)
+        self.assertTrue(form.is_valid())
 
     def test_contact_page_returns_correct_html(self):
         response = self.client.get('/contact/')
         self.assertTemplateUsed(response, 'contact.html')
 
-    def test_contact_page_returns_basic_form(self):
+    def test_contact_page_returns_correct_contact_form_html(self):
         response = self.client.get('/contact/')
         html = response.content.decode('utf8')
-        self.assertIn('<form id="contact" action="" method="post">', html)
-        self.assertIn('<input placeholder="Your name" type="text" tabindex="1" required autofocus>', html)
-        self.assertIn('<input placeholder="Your Email Address" type="email" tabindex="2" required>', html)
-        self.assertIn('<input placeholder="Your Phone Number" type="tel" tabindex="3" required>', html)
-        self.assertIn('<textarea placeholder="Type your Message Here...." tabindex="5" required></textarea>', html)
-        self.assertIn('<button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>', html)
+        self.assertIn('<form method="post">', html)
+        self.assertIn('<h2>Contact us by filling out the form below</h2>', html)
+        self.assertIn('<fieldset>', html)
+        self.assertIn('</fieldset>', html)
+        self.assertIn('<div class="form-actions">', html)
+        self.assertIn('<button type="submit">Send</button>', html)

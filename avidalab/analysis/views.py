be47f3b40eae2244e1ba1c0cc2e5e6aa97f20548
AdamSJohnson <Adam.Johnson.main@gmail.com>
2017-04-24 20:55:38 -0400

    Updated the view for Analysis to be able to pass data from the view to the template.
    We should now be able to make a function to grab the number of runs, and the data file names.
    
    Be sure to setup the dummy data for testing!

--- avidalab/analysis/views.py
+++ avidalab/analysis/views.py
@@ -20,3 +20,21 @@ class analysisDetail(DetailView):
     model = Project
     template_name = 'analysisDetail.html'
     fields = '__all__'
+
+
+
+    #So before we go into the view we want to make sure we pass enough data to it!
+    # This can be done by adding a new entry into the context array and assigning it values
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(analysisDetail, self).get_context_data(**kwargs)
+
+        #Here is where we could get the list of runs form the project
+        # For proof of concept right now the runs are just a list of 1 2 3
+        context['runs'] = ['1', '2', '3']
+        # and the data files are just A B C
+        context['data_files'] = ['A', 'B', 'C']
+
+        #Look at analysisDetail.html to see how to access the items in the list
+        return context

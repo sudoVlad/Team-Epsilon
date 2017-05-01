b9bf94cc1bddeb305959a085cd67f18c5ed414f7
Jim Hawkins <hawkinja199@potsdam.edu>
2017-05-01 16:46:31 -0400

    im a moron fixed filebrowser simplified code. basically what ive been working on streamlining this.

--- avidalab/projects/unzip.py
+++ avidalab/projects/unzip.py
@@ -1,64 +1,18 @@
-from filebrowser.fields import FileBrowseField
-from filebrowser.views import filebrowser_post_upload
-import sys, zipfile, os, os.path
-import shutil
-        
-class Media(models.Model):
-    #...
-    file = FileBrowseField("File", max_length=200, directory="shells/",  blank=True, 
-        null=True,help_text=".tar.gz")
-    #...
-    
-        
-    def post_upload_callback(sender, **kwargs):
-        """
-        Signal receiver called each time an upload has finished.
-        Triggered by Filebrowser's filebrowser_post_upload signal:
-        http://code.google.com/p/django-filebrowser/wiki/signals .
-        We'll use this to unzip .zip files in place when/if they're uploaded.
-        """
+import os, zipfile
 
-        if kwargs['file'].extension == ".tar.gz":
-            # Note: this doesn't test for corrupt zip files. 
-            # If encountered, user will get an HTTP Error 
-            # and file will remain on the server.
+dir_name = os.curdir
+extension = ".zip"
 
-            # We get returned relative path names from Filebrowser
-            path = kwargs['path'] 
-            thefile = kwargs['file'] 
-            
-            # Convert file and dir into absolute paths
-            fullpath = os.path.join(settings.MEDIA_ROOT,thefile.path_relative)
-            dirname = os.path.dirname(fullpath)
-            
-            # Get a real Python file handle on the uploaded file
-            fullpathhandle = open(fullpath, 'r') 
+os.chdir(dir_name) # change directory from working dir to dir with files
 
-            # Unzip the file, creating subdirectories as needed
-            zfobj = zipfile.ZipFile(fullpathhandle)
-            for name in zfobj.namelist():
-                if name.endswith('/'):
-                    try: # Don't try to create a directory if exists
-                        os.mkdir(os.path.join(dirname, name))
-                    except:
-                        pass
-                else:
-                    outfile = open(os.path.join(dirname, name), 'wb')
-                    outfile.write(zfobj.read(name))
-                    outfile.close()
-                
-            # Now try and delete the uploaded .zip file and the 
-            # stub __MACOSX dir if they exist.
-            try:
-                os.remove(fullpath)
-            except:
-                pass
-                
-            try:
-                osxjunk = os.path.join(dirname,'__MACOSX')
-                shutil.rmtree(osxjunk)
-            except:
-                pass                
-            
-    # Signal provided by FileBrowser on every successful upload. 
-    filebrowser_post_upload.connect(post_upload_callback)
+for item in os.listdir(dir_name): # loop through items in dir
+    if item.endswith(extension): # check for ".zip" extension
+        file_name = os.path.abspath(item) # get full path of files
+        zip_ref = zipfile.ZipFile(file_name) # create zipfile object
+        zip_ref.extractall(dir_name) # extract file to dir
+        zip_ref.close() # close file
+        #os.remove(file_name) # delete zipped file
+
+   # if item.endswith(".gz"):
+    #    file_name=os.path.abspath(item)
+

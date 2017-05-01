38e7b36363062200bf258cb06b280c2069ac63d1
Anthony <noceria198@potsdam.edu>
2017-04-30 20:44:22 -0400

    added list if runs in the decompressed field, and a list of data files in the decompressed field

--- avidalab/analysis/views.py
+++ avidalab/analysis/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from os import listdir
+from os import walk
 from django.views.generic import UpdateView , DeleteView, ListView, CreateView, DetailView
 from projects.models import Project
 
@@ -30,11 +32,19 @@ class analysisDetail(DetailView):
         # Call the base implementation first to get a context
         context = super(analysisDetail, self).get_context_data(**kwargs)
 
-        #Here is where we could get the list of runs form the project
-        # For proof of concept right now the runs are just a list of 1 2 3
-        context['runs'] = ['1', '2', '3']
-        # and the data files are just A B C
-        context['data_files'] = ['A', 'B', 'C']
+        context['runs'] = listdir(context['project'].decompressed)
+        # go through the directory at decompressed
+        # and pull out any files that end with .dat
+        tempList = []
+        # should be in the selected runs only
+        #selectedRuns= ['run 1']
+        # but right now goes through all of decompressed's files
+        for path, subdirs, files in walk(context['project'].decompressed):
+            for name in files:
+                path.join(name)
+                if(name.endswith('.dat')):
+                    tempList.append(name)
+        context['data_files'] = tempList
 
         #Look at analysisDetail.html to see how to access the items in the list
         return context

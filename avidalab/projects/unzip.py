ec23f83f81411f9394f26298cb195b4a6eb4ca45
Jim Hawkins <hawkinja199@potsdam.edu>
2017-05-02 17:53:13 -0400

    i got the .tar.gz unzip working :)

--- avidalab/projects/unzip.py
+++ avidalab/projects/unzip.py
@@ -1,7 +1,8 @@
-import os, zipfile
+import os, zipfile, gzip
 
 dir_name = os.curdir
 extension = ".zip"
+extension2 = ".tar.gz"
 
 os.chdir(dir_name) # change directory from working dir to dir with files
 
@@ -13,6 +14,12 @@ for item in os.listdir(dir_name): # loop through items in dir
         zip_ref.close() # close file
         #os.remove(file_name) # delete zipped file
 
-   # if item.endswith(".gz"):
-    #    file_name=os.path.abspath(item)
+    if item.endswith(extension2): # check for .gz 
+        file_name2 = os.path.abspath(item) # get path for file
+        inF = gzip.GzipFile(file_name2, 'rb')
+        s = inF.read()
+        inF.close()
 
+        outF = file(file_name2, 'wb')
+        outF.write(s)
+        outF.close()

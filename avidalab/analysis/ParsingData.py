713499a2c3acaa71738c41e60cdc71ab82e15eb0
tangct197 <christhtang@gmail.com>
2017-05-17 11:42:20 -0400

    Finished graphing functions which take in a list of dictionaries(data from the data files) and fieldName as argument then plots and saves the image to the /static/graphs/ directory.

---
+++ avidalab/analysis/ParsingData.py
@@ -0,0 +1,66 @@
+import re
+
+#Function for parsing the file for the column names
+#Passing file name as argument
+def parseFile(file):
+    #Opening file
+    with open(file, 'r') as f:
+        #Array for name of Columns
+        colNames = []
+
+        for line in f:
+            #Regex matching to find column 1 - X
+            match = re.search(r'^#\s*[0-9]*:', line)
+            if match:
+                #Stripping the line and adding it to the array
+                index = line.index(':')
+                columnName = line[index + 1:]
+                #Strip the newline carriage
+                columnName = columnName.strip('\n')
+                #Strip the whitespace from the beginning and end of string
+                columnName = columnName.strip()
+                #Adding columns to the array
+                colNames.append(columnName)
+        f.close()
+        #Returns array of column names
+        return colNames
+
+#Function for parsing the file for the actual data that will be returned as a dictionary
+#Passing file name as argument
+def mapData(file):
+    #Array of column names
+    columns = parseFile(file)
+
+    #Initial dictionary of column names(keys) set to empty lists(values)
+    dictOfData = {key: [] for key in columns}
+
+    #Initialize array with all the keys
+    keys = []
+    for key in dictOfData:
+        keys.append(key)
+
+    #Opening file
+    with open(file, 'r') as f:
+        for line in f:
+            #Kill comments
+            if line.startswith('#'):
+                continue
+            #kill blank lines
+            if line == '\n':
+                continue
+
+            #Taking a line of data and breaking it into a list
+            #where each index of the list refers to each data column
+            data = line.split(" ")
+            #initialized i to 0 to access the keys
+            i = 0
+            #Parse through the list of data and put data at each index into
+            #the dictionary for each key
+            for column in data:
+                #Strip the newline carriage because the last column will have one
+                column = column.strip('\n')
+                dictOfData[keys[i]].append(column)
+                i += 1
+        f.close()
+        #Return dictionary of the keys(name of each column) mapped to its values(list of data for each column)
+        return dictOfData
---
+++ avidalab/analysis/graphData.py
@@ -0,0 +1,198 @@
+#For grabbing the dictionary with data
+from ParsingData import parseFile, mapData
+
+#For graphing purposes
+import matplotlib.pyplot as plot
+import pylab
+
+#To use the numpy library
+import numpy as np
+
+#For regex purposes
+import re
+
+#For creating the /static/graphs/ directory to save the images files
+import os
+
+#Grab the directory name analysis and then create the the static/graphs/ directory under it
+#for the images to be saved in
+script_dir = os.path.dirname(__file__)
+results_dir = os.path.join(script_dir, 'static/graphs/')
+if not os.path.isdir(results_dir):
+    os.makedirs(results_dir)
+
+# mean array maker takes an array of arrays and creates an array that contains the mean array as defined below
+# [1, 2, 3], [4, 5, 6], [7, 8, 9]
+# the result would be [6, 7.5, 9]
+#
+def mean_array_maker(arrays):
+    result = []
+    if len(arrays) == 0:
+        return result
+
+    min_size = len(arrays[0])
+
+    #grab the min size to make all arrays the same size
+    for array in arrays:
+        min_size = min(min_size, len(array))
+    # make all arrays the same size
+    for array in arrays:
+        result.append(array[:min_size])
+
+    #do the mean array stuff
+    np_arrays = np.array(result)
+    mean_array = np_arrays.mean(axis=0)
+    return mean_array
+
+#Function for graphing scatter plot using the dictionary
+def graphScatter(listOfDictionaries, fieldName):
+    #Initiated empty list of arrays, this will contain a list of all the
+    #lists that are of the same fieldName
+    listOfArrays = []
+
+    #Loop through the list of dictionaries to get a list of the keys, from
+    #the keys we will then check for a match
+    for dictionary in listOfDictionaries:
+        listOfKeys = list(dictionary.keys())
+
+        #Looping through the list of keys(field names aka column names), if it matches
+        #we will then cast that list to floats then add the list to our list of arrays(aka list of lists)
+        for key in listOfKeys:
+            m = re.match(fieldName, key, re.IGNORECASE)
+            if bool(m):
+                listOfArrays.append(list(map(float, dictionary[key])))
+
+    #The mean list(array) of all the lists of the same fieldName
+    averageOfLists = mean_array_maker(listOfArrays).tolist()
+
+    #Time array to graph against time
+    timeArray = []
+    i = 1;
+    for n in averageOfLists:
+        timeArray.append(i)
+        i += 1
+
+    #Labeling the title, x-axis, y-axis, then plotting
+    plot.title('Time vs ' + fieldName)
+    plot.xlabel(fieldName)
+    plot.ylabel('Time')
+    plot.scatter(averageOfLists, timeArray)
+
+    #Displaying the graph
+    #plot.show()
+
+    #Saving the plotted graph to /static/graphs/scatter_plot.png
+    plot.savefig(results_dir + 'scatter_plot.png')
+
+#Function for graphing histogram using the dictionary
+def graphHistogram(listOfDictionaries, fieldName):
+    #Initiated empty list of arrays, this will contain a list of all the
+    #lists that are of the same fieldName
+    listOfArrays = []
+
+    #Loop through the list of dictionaries to get a list of the keys, from
+    #the keys we will then check for a match
+    for dictionary in listOfDictionaries:
+        listOfKeys = list(dictionary.keys())
+
+        #Looping through the list of keys(field names aka column names), if it matches
+        #we will then cast that list to floats then add the list to our list of arrays(aka list of lists)
+        for key in listOfKeys:
+            m = re.match(fieldName, key, re.IGNORECASE)
+            if bool(m):
+                listOfArrays.append(list(map(float, dictionary[key])))
+
+    #The mean list(array) of all the lists of the same fieldName
+    averageOfLists = mean_array_maker(listOfArrays).tolist()
+
+    #Labeling the title, x-axis, then plotting
+    plot.title('Histogram of ' + fieldName)
+    plot.xlabel(fieldName)
+    plot.hist(averageOfLists, bins=30)
+
+    #Saving the plotted graph to /static/graphs/histogram.png
+    plot.savefig(results_dir + 'histogram.png')
+
+    #Displaying the graph
+    #plot.show()
+
+#Function for graphing the line graph using the dictionary
+def graphLineGraph(listOfDictionaries, fieldName):
+    #Initiated empty list of arrays, this will contain a list of all the
+    #lists that are of the same fieldName
+    listOfArrays = []
+
+    #Loop through the list of dictionaries to get a list of the keys, from
+    #the keys we will then check for a match
+    for dictionary in listOfDictionaries:
+        listOfKeys = list(dictionary.keys())
+
+        #Looping through the list of keys(field names aka column names), if it matches
+        #we will then cast that list to floats then add the list to our list of arrays(aka list of lists)
+        for key in listOfKeys:
+            m = re.match(fieldName, key, re.IGNORECASE)
+            if bool(m):
+                listOfArrays.append(list(map(float, dictionary[key])))
+
+    #The mean list(array) of all the lists of the same fieldName
+    averageOfLists = mean_array_maker(listOfArrays).tolist()
+
+    #Labeling the title, x-axis, then plotting
+    plot.title('Line graph of ' + fieldName + ' vs Time')
+    plot.xlabel('Time')
+    plot.ylabel(fieldName)
+    plot.plot(averageOfLists)
+
+    #Saving the plotted graph to /static/graphs/line_graph.png
+    plot.savefig(results_dir + 'line_graph.png')
+
+    #Displaying the graph
+    #plot.show()
+
+#Function for graphing the box and whisker plot using the dictionary
+def graphBoxAndWhisker(listOfDictionaries, fieldName):
+    #Initiated empty list of arrays, this will contain a list of all the
+    #lists that are of the same fieldName
+    listOfArrays = []
+
+    #Loop through the list of dictionaries to get a list of the keys, from
+    #the keys we will then check for a match
+    for dictionary in listOfDictionaries:
+        listOfKeys = list(dictionary.keys())
+
+        #Looping through the list of keys(field names aka column names), if it matches
+        #we will then cast that list to floats then add the list to our list of arrays(aka list of lists)
+        for key in listOfKeys:
+            m = re.match(fieldName, key, re.IGNORECASE)
+            if bool(m):
+                listOfArrays.append(list(map(float, dictionary[key])))
+
+    #The mean list(array) of all the lists of the same fieldName
+    averageOfLists = mean_array_maker(listOfArrays).tolist()
+
+    plot.title('Box and Whisker Plot of ' + fieldName)
+    plot.ylabel(fieldName)
+    plot.boxplot(averageOfLists)
+
+    #Saving the plotted graph to /static/graphs/box_and_whisker_plot.png
+    plot.savefig(results_dir + 'box_and_whisker_plot.png')
+
+    #Displaying the graph
+    #plot.show()
+
+#Function for testing the graph functions
+def TestToShow():
+    #Testing functions by getting a dictionary(data from a .dat file)
+    dictionaryOfData = mapData("dominant.dat")
+    dictionaryOfData2 = mapData("count.dat")
+
+    #Appending dictionaries to listOfDicts
+    listOfDicts = []
+    listOfDicts.append(dictionaryOfData)
+    listOfDicts.append(dictionaryOfData2)
+
+    #Testing graphs with list of dictionaries and fieldname(column name)
+    #graphScatter(listOfDicts, 'Update')
+    #graphHistogram(listOfDicts, 'Update')
+    #graphLineGraph(listOfDicts, 'Update')
+    #graphBoxAndWhisker(listOfDicts, 'Update')
